d6ef1dcf7f1e8adb5bac6ff74d8a2124e484e194
Polman3 <polman3@o2.pl>
2017-11-13 13:36:45 +0100

    Added getters and setters

--- src/Animal.java
+++ src/Animal.java
@@ -1,7 +1,7 @@
 public class Animal {
-    private
-    String name, type, race, health, sex;
-    int age;
+
+    private String name, type, race, health, sex;
+    private int age;
 
     public
     Animal(String nazwa, String typ, String rasa, String stan_zdrowia, String plec, int wiek){
@@ -12,28 +12,41 @@ public class Animal {
         this.sex = plec;
         this.age = wiek;
     }
-
+    //Getters and setters
     public String getName(){
         return name;
     }
-
+    public void setName(String nazwa){
+        this.name = nazwa;
+    }
     public String getType(){
         return type;
     }
-
+    public void setType(String typ){
+        this.type = typ;
+    }
     public String getRace(){
         return race;
     }
-
+    public void setRace(String rasa){
+        this.race = rasa;
+    }
     public String getHealth(){
         return health;
     }
-
+    public void setHealth(String stan_zdrowia){
+        this.health = stan_zdrowia;
+    }
     public String getSex(){
         return sex;
     }
-
+    public void setSex(String plec){
+        this.sex = plec;
+    }
     public int getAge() {
         return age;
+    }
+    public void setAge(int wiek){
+        this.age = wiek;
     }
 }
