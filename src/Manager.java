673657eb11a188b0f6123389bed29f6a18db46a7
Polman3 <polman3@o2.pl>
2017-11-13 16:40:45 +0100

    Added void functions

--- src/Manager.java
+++ src/Manager.java
@@ -1,7 +1,11 @@
 import java.util.Scanner;
-import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Manager {
+
+    static int quantity;
+    static List<String> lista = new ArrayList<String>();
 
     public static void addAnimal(){
         String nazwa, typ, rasa, stan_zdrowia, plec;
@@ -21,51 +25,57 @@ public class Manager {
         wiek = odczyt_danych.nextInt();
 
         Animal zwierzatko = new Animal(nazwa,typ,rasa,stan_zdrowia,plec,wiek);
-        System.out.println(zwierzatko.getName() + zwierzatko.getType() + zwierzatko.getRace() + zwierzatko.getHealth() + zwierzatko.getSex() + zwierzatko.getAge());
+        System.out.println(zwierzatko.getName() +" "+ zwierzatko.getType() +" "+ zwierzatko.getRace() +" "+ zwierzatko.getHealth() +" "+ zwierzatko.getSex() +" "+ zwierzatko.getAge()+"\n");
+    }
+
+    public static void getMenu(){
+        System.out.println("1. Dodaj zwierzątko");
+        System.out.println("2. Usun zwierzątko");
+        System.out.println("3. Edytuj dane");
+        System.out.println("4. Status schroniska");
+        System.out.println("5. Wyjście");
+        Scanner odczyt = new Scanner(System.in);
+        char answer;
+        answer = odczyt.next().charAt(0);
+
+        switch (answer) {
+            case '1':
+                if(quantity>0){
+                    addAnimal();
+                    quantity--;
+                }
+                else{
+                    System.out.println("Brak wolnych miejsc w schronisku");
+                    System.out.println("Ilosc miejsc: "+ quantity + "/20 \n");
+                }
+                break;
+            case '2':
+                if(quantity<20){
+                    quantity++;
+                }
+                else{
+                    System.out.println("Schronisko jest puste");
+                    System.out.println("Ilosc miejsc: "+ quantity + "/20 \n");
+                }
+                break;
+            case '3':
+                break;
+            case '4':
+                break;
+            case '5':
+                System.exit(0);
+        }
     }
 
     public static void main(String[] args) {
 
-        int quantity = 20;
+        quantity = 20;
+
+        System.out.println("Witaj w programie: Manager schroniska");
+
 
         while(1==1) {
-            System.out.println("Witaj w programie: Manager schroniska");
-            System.out.println("1. Doddaj zwierzątko");
-            System.out.println("2. Usun zwierzątko");
-            System.out.println("3. Edytuj dane");
-            System.out.println("4. Status schroniska");
-            System.out.println("5. Wyjście");
-            Scanner odczyt = new Scanner(System.in);
-            char answer;
-            answer = odczyt.next().charAt(0);
-
-            switch (answer) {
-                case '1':
-                    if(quantity>0){
-                        addAnimal();
-                        quantity--;
-                    }
-                    else{
-                        System.out.println("Brak wolnych miejsc w schronisku");
-                        System.out.println("Ilosc miejsc: "+ quantity + "/20 \n");
-                    }
-                    break;
-                case '2':
-                    if(quantity<20){
-                        quantity++;
-                    }
-                    else{
-                        System.out.println("Schronisko jest puste");
-                        System.out.println("Ilosc miejsc: "+ quantity + "/20 \n");
-                    }
-                    break;
-                case '3':
-                    break;
-                case '4':
-                    break;
-                case '5':
-                    System.exit(0);
-            }
+            getMenu();
         }
     }
 
